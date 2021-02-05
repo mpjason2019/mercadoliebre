9dcb329aeda1ef35b90737dc8d6e3997d9d4d7e2
Jeisson Medina <medinapjason@gmail.com>
2021-02-05 20:12:27 -0300

    proyecto mercadoliebre

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bodyParser = require('body-parser')
+
+// const publicpath = path.resolve('public');
+app.use(express.static('./public'));
+// app.use(express.static('./views'));
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.get('/', (req, res) =>{
+    res.sendFile( path.resolve ('./views/home.html'));
+
+});
+app.get('/ofertas', (req, res) =>{
+    res.sendFile( path.resolve ('./views/ofertas.html'));
+
+});
+app.get('/tiendas_oficiales', (req, res) =>{
+    res.sendFile( path.resolve ('./views/tiendas_oficiales.html'));
+
+});app.get('/vender', (req, res) =>{
+    res.sendFile( path.resolve ('./views/vender.html'));
+});
+
+app.get('/ayuda', (req, res) =>{
+    res.sendFile( path.resolve ('./views/ayuda.html'));
+});
+
+app.get('/crea_tu_cuenta', (req, res) =>{
+    res.sendFile( path.resolve ('./views/crea_tu_cuenta.html'));
+});
+
+
+
+
+app.get('/ingresa', (req, res) =>{
+    res.sendFile( path.resolve ('./views/ingresa.html'));
+});
+
+app.get('/mis_compras', (req, res) =>{
+    res.sendFile( path.resolve ('./views/mis_compras.html'));
+});
+
+app.listen (3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+
